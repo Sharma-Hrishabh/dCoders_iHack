be04f58582e366176f650c0f1f0300c5d27e80b7
Sharma-Hrishabh <30hrs1999@gmail.com>
2019-01-19 12:40:28 +0530

    constructor setup

--- contracts/dCoders.sol
+++ contracts/dCoders.sol
@@ -2,6 +2,20 @@ pragma solidity >=0.4.22;
 
 
 contract dCoders {
+  address public organiser;
+  uint public numRegistrants;
+  uint private regFee;
+  event Register(address _from);
+
   constructor() public {
+    organiser = msg.sender;
+    numRegistrants = 0;
+    regFee = 2 wei;
   }
+
+
+
+
+
+
 }
