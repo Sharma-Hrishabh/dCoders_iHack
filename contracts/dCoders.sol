9453b1fd326860fc3ea9586f85ebe4fb4aaf3551
Sharma-Hrishabh <30hrs1999@gmail.com>
2019-01-19 13:51:55 +0530

    minor changes

--- contracts/dCoders.sol
+++ contracts/dCoders.sol
@@ -5,19 +5,31 @@ contract dCoders {
   address public organiser;
   uint public numRegistrants;
   uint public regFee;
-  event Register(address indexed _from);
+  event Register(address indexed _from,uint indexed _amount);
   mapping (address => uint) public balance;  
 
   constructor() public {
     organiser = msg.sender;
     numRegistrants = 0;
-    regFee = 2 wei;
+    regFee = 200000000000000000 wei;
   }
 
   function () payable external {
- require(msg.value == 2 wei);
+//  require(msg.value == regFee);
+uint256 cash = msg.value;
+if(cash != regFee)  {return;}
+
+// if(msg.value != regFee) {return;}
+else{
  balance[address(this)] += regFee;     
- emit Register(msg.sender);  
+ emit Register(msg.sender,msg.value);  
+  }
 }
+
+
+
+
+
+
 
 }
