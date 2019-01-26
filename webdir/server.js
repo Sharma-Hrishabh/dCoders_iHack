9c9a5e864f784ec9ab6b7f5375dd756974b9e445
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2019-01-26 12:58:21 +0530

    interconnect all pages

--- webdir/server.js
+++ webdir/server.js
@@ -7,11 +7,23 @@ const port = 3000
 app.use(express.static(path.join(__dirname+'/static/js/')))
 app.use(express.static(path.join(__dirname+'/templates/')))
 app.use(express.static(path.join(__dirname+'/static/css')))
+app.use(express.static(path.join(__dirname+'/static')))
 
 // the root page content
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname+'/templates/index.html'))
 })
-// app.get('/abc', (req, res) => res.sendFile('css.html'))
+
+app.get('/home', function (req, res) {
+    res.sendFile(path.join(__dirname+'/templates/index.html'))
+})
+
+app.get('/timer', function (req, res) {
+    res.sendFile(path.join(__dirname+'/templates/timer.html'))
+})
+
+app.get('/ide', function (req, res) {
+    res.sendFile(path.join(__dirname+'/templates/ide.html'))
+})
 
 app.listen(port, () => console.log(__dirname+`\nExample app listening on port ${port}!`))
