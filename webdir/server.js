c79c7a025f25b32267e762f9c71d89cdb32c0a0e
Sharma-Hrishabh <30hrs1999@gmail.com>
2019-01-26 18:50:58 +0530

    after mustache config

--- webdir/server.js
+++ webdir/server.js
@@ -3,15 +3,33 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 // the static directories
-app.use(express.static(path.join(__dirname+'/static/js/')))
-app.use(express.static(path.join(__dirname+'/templates/')))
-app.use(express.static(path.join(__dirname+'/static/css')))
-app.use(express.static(path.join(__dirname+'/static')))
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','mustache')
+app.engine('mustache',require('hogan-middleware').__express)
+app.use(express.static(path.join(__dirname,'public')))
+
+// app.use(express.static(path.join(__dirname+'/static/js/')))
+// app.use(express.static(path.join(__dirname+'/templates/')))
+// app.use(express.static(path.join(__dirname+'/static/css')))
+// app.use(express.static(path.join(__dirname+'/static')))
 
 // the root page content
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname+'/templates/index.html'))
+    // res.sendFile(path.join(__dirname+'/templates/index.html'))
+    res.render('index',null)
 })
 
 app.get('/home', function (req, res) {
