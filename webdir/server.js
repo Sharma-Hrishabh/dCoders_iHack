f10079356c4a75d76a6778d3d3781d5280a63192
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2019-01-27 19:22:04 +0530

    store transaction in db

--- webdir/server.js
+++ webdir/server.js
@@ -7,24 +7,26 @@ const port = 3000
 
 // connect to mysql db
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'ujjwal',
-  password : 'root',
-  database : 'newdb'
+    host: 'localhost',
+    user: 'ujjwal',
+    password: 'root',
+    database: 'newdb'
 });
 
 connection.connect()
 
-connection.query('SELECT * from tb;', function (err, rows, fields) {
-  if (err) throw err
+// connection.query('SELECT * from tb;', function (err, rows, fields) {
+//   if (err) throw err
+// 
+//   console.log('The return is: ', rows[0].id, rows[0].name)
+// })
 
-  console.log('The return is: ', rows[0].id, rows[0].name)
-})
-
-connection.end()
+// connection.end()
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 // // create application/json parser
 // var jsonParser = bodyParser.json()
 // 
@@ -32,42 +34,67 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // the static directories
-app.use(express.static(path.join(__dirname+'/static/js/')))
-app.use(express.static(path.join(__dirname+'/templates/')))
-app.use(express.static(path.join(__dirname+'/static/css')))
-app.use(express.static(path.join(__dirname+'/static')))
+app.use(express.static(path.join(__dirname + '/static/js/')))
+app.use(express.static(path.join(__dirname + '/templates/')))
+app.use(express.static(path.join(__dirname + '/static/css')))
+app.use(express.static(path.join(__dirname + '/static')))
 
 // the root page content
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname+'/templates/index.html'))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/templates/index.html'))
 })
 
-app.all('/home', function (req, res) {
+app.all('/home', function(req, res) {
     if (req.method == "GET")
-        res.sendFile(path.join(__dirname+'/templates/index.html'))
-    else res.send(req.method+" request not supported.")
+        res.sendFile(path.join(__dirname + '/templates/index.html'))
+    else res.send(req.method + " request not supported.")
 })
 
-app.all('/timer', function (req, res) {
+app.all('/timer', function(req, res) {
     if (req.method == "GET")
-        res.sendFile(path.join(__dirname+'/templates/timer.html'))
-    else res.send(req.method+" request not supported.")
+        res.sendFile(path.join(__dirname + '/templates/timer.html'))
+    else res.send(req.method + " request not supported.")
 })
 
-app.all('/ide', function (req, res) {
+app.all('/ide', function(req, res) {
     if (req.method == "GET")
-        res.sendFile(path.join(__dirname+'/templates/ide.html'))
-    else res.send(req.method+" request not supported.")
+        res.sendFile(path.join(__dirname + '/templates/ide.html'))
+    else res.send(req.method + " request not supported.")
 })
 
-app.post('/userreg', function (req, res) {
-    console.log(req.body.account)
+app.post('/userreg', function(req, res) {
+    console.log('POST userreg: ' + req.body.account)
     if (req.body.account == null)
-        res.send({status: 0});
+        res.send({
+            status: 0
+        })
     else {
-        res.send({status: 1})
+        res.send({
+            status: 3
+        })
     }
     // return res.send(req.body)
 })
 
-app.listen(port, () => console.log(__dirname+`\nExample app listening on port ${port}!`))
+app.post('/posttx', function(req, res) {
+    if (req.body.txid == null || req.body.account == null)
+        res.send({
+            error: "Transaction id not provided."
+        })
+    else {
+        // TODO: insert into database
+        connection.query('insert into participants values (\'' +
+            req.body.account +
+            '\', (select id from events order by time), \'' +
+            req.body.txid +
+            '\', (select now()), 0);',
+            function(err, rows, fields) {
+                if (err) throw err;
+            })
+        res.send({
+            status: "Success"
+        })
+    }
+})
+
+app.listen(port, () => console.log('')) //__dirname+`\nExample app listening on port ${port}!`))
--- webdir/static/js/app.js
+++ webdir/static/js/app.js
@@ -6,22 +6,28 @@ App = {
 
     register: function(event) {
         event.preventDefault();
-        $('#btn1').attr('disabled','disabled');
-        web3.eth.sendTransaction({from:web3.eth.defaultAccount, to:'0xad8e3F5D68E9529FF01A7bD17D00ba4Bab14A2D3', value: web3.toWei(0.0001, 'ether'), gasLimit: 24000, gasPrice: 20000000000},
-        function(error, result) {
-            if (!error) {
-                txID = result;
-                monitor();
-            }
-            else {
-                console.log(error);
-                alert(""+error);
-            }
-        })
-      },
+        $('#btn1').attr('disabled', 'disabled');
+        web3.eth.sendTransaction({
+                from: web3.eth.defaultAccount,
+                to: '0xad8e3F5D68E9529FF01A7bD17D00ba4Bab14A2D3',
+                value: web3.toWei(0.0001, 'ether'),
+                gasLimit: 24000,
+                gasPrice: 20000000000
+            },
+            function(error, result) {
+                if (!error) {
+                    txID = result;
+                    postTx(txID);
+                    monitor();
+                } else {
+                    console.log(error);
+                    alert("" + error);
+                }
+            })
+    },
 
     initWeb3: async function() {
-        if(typeof web3 !== undefined) {
+        if (typeof web3 !== undefined) {
             try {
                 App.web3Provider = web3.currentProvider;
                 App.web3Provider.enable();
@@ -30,15 +36,14 @@ App = {
             } catch (e) {
                 alert(e);
             }
-        }
-        else {
+        } else {
             alert("MetaMask not found! Working on localhost:7545.");
             App.web3Provider = new web3.providers.HttpProvider("http://localhost:7545");
         }
-  
+
         web3 = new Web3(App.web3Provider);
-  
-      return;
+
+        return;
     },
 
     bindEvents: function() {
@@ -51,15 +56,16 @@ function sleep(time) {
     return new Promise((resolve) => setTimeout(resolve, time));
 }
 
+// monitor the transaction on client-browser
 async function monitor() {
     var status = 0;
     while (status != 1) {
         // console.log(txID);
-        web3.eth.getTransactionReceipt(txID,function(error, result) {
+        web3.eth.getTransactionReceipt(txID, function(error, result) {
             if (!error) {
                 try {
                     status = parseInt(result.status, 16);
-                    if (status == 1){
+                    if (status == 1) {
                         window.location.href = "timer";
                         return;
                     }
@@ -74,23 +80,31 @@ async function monitor() {
     }
 }
 
+// check if user is already registered
 function post(account) {
     var data = JSON.stringify({
-      "account": account
+        "account": account
     });
 
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
 
-    xhr.addEventListener("readystatechange",async function () {
-      if (this.readyState === 4) {
-        if (JSON.parse(this.responseText).status == 1) window.location.href = "timer";
-        else if (JSON.parse(this.responseText).status == 0) {
-            // alert("retrying in 3 seconds");
-            await sleep(3000);
-            post(web3.eth.defaultAccount);
+    xhr.addEventListener("readystatechange", async function() {
+        if (this.readyState === 4) {
+            var status = JSON.parse(this.responseText).status;
+            if (status == 1) window.location.href = "timer";
+            else if (status == 0) {
+                // alert("retrying in 3 seconds");
+                await sleep(1000);
+                post(web3.eth.defaultAccount);
+            } else if (status == 2) {
+                alert("Transaction still pending!\nPlease wait for confirmation.");
+                // monitor()
+            } else {
+                App.bindEvents();
+                document.getElementById('btn1').style.display = "block";
+            }
         }
-      }
     });
 
     xhr.open('POST', 'userreg');
@@ -99,8 +113,24 @@ function post(account) {
     xhr.send(data);
 }
 
+// post the transaction for logging in server
+function postTx() {
+    var data = JSON.stringify({
+        "account": web3.eth.defaultAccount,
+        "txid": txID
+    });
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.open('POST', 'posttx');
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.send(data);
+}
+
 $(function() {
-$(window).load(function() {
-    App.initWeb3();
-});
+    $(window).load(function() {
+        App.initWeb3();
+    });
 });
